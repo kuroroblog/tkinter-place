000619de91146e7beca0edbbac13739ce7e2de8c
tocchimittsu <arimagedon21@gmail.com>
2021-06-14 10:26:14 +0900

    修正

--- main.py
+++ main.py
@@ -10,42 +10,37 @@ class Application(tk.Frame):
         # geometryについて : https://kuroro.blog/python/rozH3S2CYE0a0nB3s2QL/
         self.master.geometry("300x200")
 
-        # Window内にFrameを作成する。
-        # 親要素をWindowとする。
-        # width : Frameの幅を設定する。
-        # height : Frameの高さを設定する。
-        # bg : 背景色を設定する。
+        # Windowを親要素として、frame Widget(Frame)を作成する。
+        # width : 幅を設定
+        # height : 高さを設定
+        # borderwidth : 枠の幅を設定
+        # bg : 背景色を設定
         # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
-        # borderwidth : 境界線の幅を指定する。
-        # relief : 境界線のデザインを設定する。
+        # relief : 枠のデザインを設定する。
         # reliefについて : https://cercopes-z.com/Python/stdlib-tkinter-widget-frame-py.html#detail-relief
         # Frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
-        frame = tk.Frame(self.master, width=400, height=400, borderwidth=20,
-                         bg="green", relief=tk.SUNKEN)
+        frame = tk.Frame(self.master, width=400, height=400, borderwidth=20, bg="green", relief=tk.SUNKEN)
 
-        # ラベルWidgetを作成する。
+        # Windowを親要素として、Frameの配置を行う。
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        frame.pack()
+
+        # frame Widget(Frame)を親要素として、label Widgetを作成する。
         # text : テキスト情報
         # width : 幅の設定
         # height : 高さの設定
         # bg : 背景色の設定
         # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
-        # relief : 境界線の設定
-        # reliefについて : https://cercopes-z.com/Python/stdlib-tkinter-widget-frame-py.html#detail-relief
         # Labelについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
-        label = tk.Label(frame, text="1", width=30, height=15,
-                         bg="orchid4", relief="raised")
+        label = tk.Label(frame, text="label", width=30, height=15, bg="blue")
 
-        # packを利用して、Frameの配置を行う。
-        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
-        frame.pack()
-
-        # Frameを親要素とした場合に、ラベルWidgetをどのように配置するのか?
-        label.place(x=40, y=40)
-
+        # frame Widget(Frame)を親要素とした場合に、label Widgetをどのように配置するのか?
+        label.place(x=0, y=0)
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = Application(master=root)
+
     # Windowをループさせて、継続的にWindow表示させる。
     # mainloopについて : https://kuroro.blog/python/DmJdUb50oAhmBteRa4fi/
     app.mainloop()
